b867d806e7332626e91ee2f7662265eecc88d3c7
Shteryana Shopova <s.shopova@strem.io>
2017-08-29 05:07:39 +0000

    Add more unit tests
        * verify proper price each day except the 4th (deliberately, thus
    catching the getPrice pitfall)
        * also verifies crowdsale continues 30 days
        * verify that ETH transfer for tokens is not possible after end of
    crowdsale
        * adjust hardcap & account balances so that tests may run properly

--- test/03_bonus.js
+++ test/03_bonus.js
@@ -2,7 +2,7 @@ var ADXToken = artifacts.require("./ADXToken.sol");
 var Promise = require('bluebird')
 var time = require('../helpers/time')
 
-var startPrice = 1170 * 1000;
+var startPrice = 1170 * 10000;
 
 contract('ADXToken', function(accounts) {
 
@@ -10,14 +10,14 @@ contract('ADXToken', function(accounts) {
 
   var EXPECT_FOR_ONE_ETH = 11700000; // 900*1.3*10000
 
-  var ownerAddr = web3.eth.accounts[1];
+  var ownerAddr = web3.eth.accounts[0];
   var adexTeamAddr = web3.eth.accounts[9];
   var adexFundAddr = web3.eth.accounts[8];
 
   var participiants = web3.eth.accounts.slice(2, 8).map(account => {
     return {
       account: account,
-      sent: web3.toWei(1, 'ether')
+      sent: web3.toWei(35, 'ether')
     }
   })
 
@@ -31,10 +31,10 @@ contract('ADXToken', function(accounts) {
       adexTeamAddr, // team, whre 2% wings and 2% bounty will be received
       startDate, // public sale start
       startDate-7*24*60*60, // private sale start
-      web3.toWei(4, 'ether'), // ETH hard cap, in wei
-      web3.eth.accounts[1], 5047335,
-      web3.eth.accounts[2], 5047335, // TODO: change accordingly
-      web3.eth.accounts[3], 2340000 
+      web3.toWei(40, 'ether'), // ETH hard cap, in wei
+      web3.eth.accounts[0], 5047335,
+      web3.eth.accounts[1], 5047335, // TODO: change accordingly
+      web3.eth.accounts[2], 2340000
     )
   }
 
@@ -64,7 +64,7 @@ contract('ADXToken', function(accounts) {
           web3.eth.sendTransaction({
             from: participiant.account,
             to: crowdsale.address,
-            value: participiant.sent,
+            value: web3.toWei(1, 'ether'), //participiant.sent,
             gas: 130000
           }, (err) => {
             if (err) reject(err) 
@@ -96,23 +96,41 @@ contract('ADXToken', function(accounts) {
     }
   }
 
-  it('Should allow to send ETH in exchange of Tokens - first day', testExchange(1, EXPECT_FOR_ONE_ETH))
+  it('Should allow to send ETH in exchange of Tokens - first day', testExchange(0, EXPECT_FOR_ONE_ETH))
 
-  it('Should allow to send ETH in exchange of Tokens - first day, again', testExchange(2, EXPECT_FOR_ONE_ETH))
+  it('Should allow to send ETH in exchange of Tokens - first day, again', testExchange(1, EXPECT_FOR_ONE_ETH))
   
   it('Change time to next day', moveTime(1*24*60*60 + 30))
-  it('Should allow to send ETH in exchange of Tokens - second day', testExchange(3, EXPECT_FOR_ONE_ETH * 0.9))
+  it('Should allow to send ETH in exchange of Tokens - second day', testExchange(2, EXPECT_FOR_ONE_ETH * 0.9))
 
   it('Change time to next day', moveTime(1*24*60*60 + 30))
-  it('Should allow to send ETH in exchange of Tokens - third day', testExchange(4, EXPECT_FOR_ONE_ETH * 0.9 * 0.9))
+  it('Should allow to send ETH in exchange of Tokens - third day', testExchange(3, EXPECT_FOR_ONE_ETH * 0.9 * 0.9))
 
   //it('Change time to next day', moveTime(1*24*60*60 + 30))
   //it('Should allow to send ETH in exchange of Tokens - fourth day', testExchange(5, EXPECT_FOR_ONE_ETH * 0.9 * 0.9 * 0.9))
 
   //it('Change time to next day', moveTime(1*24*60*60 + 30))
   //it('Should allow to send ETH in exchange of Tokens - fifth day', testExchange(5, EXPECT_FOR_ONE_ETH * 0.9 * 0.9 * 0.9 * 0.9))
-  
-  it('Change time to end of crowdsale', moveTime(30*24*60*60 + 30))
 
+  var day = 4;
+  var amount = EXPECT_FOR_ONE_ETH;
+  var lastPrice = EXPECT_FOR_ONE_ETH * 0.9 * 0.9 * 0.9;
+  it('Change time to next day - skip transfers', moveTime(1 * 24*60*60));
+
+  for ( ; day < 30; day++) {
+      lastPrice = Math.floor((lastPrice * 9) / 10);
+      amount += lastPrice;
+      it('Change time to day ' + (day + 1) + ' of crowdsale', moveTime(1 * 24*60*60));
+      it('Should allow to send ETH in exchange of Tokens - ' + (day + 1) +'th day', testExchange(1, amount));
+  }
+
+  it('Change time to end of crowdsale', moveTime(1*24*60*60 + 30))
+  it("Should not allow to send ETH in exchange of Tokens after crowdsale end", function() {
+      return testExchange(1, amount + lastPrice)()
+    .then(function() { throw new Error('Cant be here'); })
+    .catch(function(err) {
+      assert.equal(err.message, 'VM Exception while processing transaction: invalid opcode');
+    })
+  })
 
 });
